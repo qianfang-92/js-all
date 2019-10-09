703de344bd8bb1f59f7329bab8f4990baa51b34e
faye <qianf92@163.com>
2019-10-09 20:21:59 +0800

    day02

---
+++ week4/day03/animate.js
@@ -0,0 +1,68 @@
+var animate = (function () {
+    var moveType = {  //各种不同的运动轨迹
+        linear:function linear(time,changeL,duration,beginL) {
+            return changeL/duration*time + beginL;
+        },
+        easeIn: function (t,c,d,b) {  
+            return -c * (Math.sqrt(1 - (t /= d) * t) - 1) + b;
+        },
+        easeOut: function (t,c,d,b) {
+            return c * Math.sqrt(1 - (t = t / d - 1) * t) + b;
+        },
+        easeInOut: function (t,c,d,b) {
+            if ((t /= d / 2) < 1) {
+                return -c / 2 * (Math.sqrt(1 - t * t) - 1) + b;
+            }
+            return c / 2 * (Math.sqrt(1 - (t -= 2) * t) + 1) + b;
+        }
+    };
+    function move2 (ele, duration, obj,type = 'linear',cb) {
+        let binObj = {};
+        for (var k in obj) {
+            binObj[k] = parseFloat(getComputedStyle(ele)[k]);  //获取集合里各个属性的初始值
+        }
+        let time = 0; //记录的是运动过的时间
+        // for (var k in binObj) {
+        //     binObj[k].l = linear()
+        // }
+
+        let l = {};
+        let timer = setInterval(()=>{
+            time += 20;
+            for (let k in binObj) {
+                l[k] = moveType[type](time,obj[k] - binObj[k], duration, binObj[k])
+            }
+            if (time >= duration) {
+                l = obj; //当前位置等于目标位置
+                clearInterval(timer);
+                cb && cb(); //回调函数存在时执行它
+            }
+            for (let k in obj) {
+                ele.style[k] = l[k] + (k === 'opacity' ? '' : 'px')
+            }
+        },20)
+
+    }
+    return function (ele, obj, duration, type, cb) {
+        if(typeof duration == 'function') {
+            cb = duration;
+            duration = 1000;
+        }
+        if (typeof time == 'function') {
+            cb = time;
+            time = 1000;
+        }
+        if(typeof type == 'function') {
+            cb = type;
+            type = 'linear';
+        }
+        if (typeof duration == 'string'){
+            type = duration;
+            duration = 1000;
+        }
+        move2(ele,duration, obj,type,cb);
+    }
+})()
+
+
+//回调函数：写函数体，别人调用
